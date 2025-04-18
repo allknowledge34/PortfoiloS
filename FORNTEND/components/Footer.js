a3467e92273f67dfea6e117c5fbf3bfb4a7a2c55
SachinKumar <166914290+allknowledge34@users.noreply.github.com>
2025-04-19 01:32:10 +0530

    Update Footer.js

--- FORNTEND/components/Footer.js
+++ FORNTEND/components/Footer.js
@@ -12,17 +12,17 @@ export default function Footer() {
                 </div>
                 <div className="ul flex gap-2">
                     <li><Link href='/services'>Services</Link></li>
-                    <li><Link href='/services'>Works</Link></li>
-                    <li><Link href='/services'>Resume</Link></li>
-                    <li><Link href='/services'>Skills</Link></li>
-                    <li><Link href='/services'>Testimonial</Link></li>
+                    <li><Link href='/projects'>Works</Link></li>
+                    <li><Link href='/'>Resume</Link></li>
+                    <li><Link href='/shop'>Shop</Link></li>
+                    <li><Link href='/blogs'>Blogs</Link></li>
                     <li><Link href='/contact'>Contact</Link></li>
                 </div>
                 <ul className="hero_social">
-                    <li><a href="/" target="_blank"><FaTwitter/></a></li>
-                    <li><a href="/" target="_blank"><LiaBasketballBallSolid/></a></li>
-                    <li><a href="/" target="_blank"><GrLinkedinOption/></a></li>
-                    <li><a href="/" target="_blank"><FaGithub/></a></li>
+                    <li> < a href="https://x.com/home?lang=en"><FaTwitter /></a></li>
+                    <li> < a href="https://www.youtube.com/@AiCodingHub"><LiaBasketballBallSolid /></a></li>
+                    <li> < a href="https://www.linkedin.com/in/sachin-kumar-355203340/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><GrLinkedinOption /></a></li>
+                    <li> < a href="https://github.com/allknowledge34"><FaGithub /></a></li>
                 </ul>
                 <div className="copyrights">&copy; 2025 All Right Reserved By <span>AiCodinghub</span></div>
             </div>
